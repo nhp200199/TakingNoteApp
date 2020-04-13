b70415d845659eca868288b1d61b5ccab8e12675
Phuc Nguyen <nhp200199@gmail.com>
2020-04-13 12:39:27 +0700

    Lesson 1 complete

---
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -0,0 +1,175 @@
+package com.example.notekeeper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+    public static final String NOTE_POSITION = "com.example.notekeeper.NOTE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
+    private NoteInfo mNote;
+    private Spinner mSpCourses;
+    private EditText mEdtNoteTitle;
+    private EditText mEdtNoteText;
+    private boolean mIsNewNote;
+    private int mNotePosition;
+    private boolean mIsCancelling;
+    private NoteActivityViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewModelProvider viewModelProvider = new ViewModelProvider(getViewModelStore(),
+                ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()));
+        mViewModel = viewModelProvider.get(NoteActivityViewModel.class);
+
+        if(savedInstanceState!=null && mViewModel.isNewlyCreated){
+            mViewModel.restoreState(savedInstanceState);
+        }
+        mViewModel.isNewlyCreated =false;
+
+        mSpCourses = findViewById(R.id.sp_courses);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                    new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        mSpCourses.setAdapter(adapterCourses);
+
+        readDisplayStateValues();
+        saveOriginalNoteValues();
+
+        mEdtNoteTitle = findViewById(R.id.edt_title);
+        mEdtNoteText = findViewById(R.id.edt_note);
+
+        if(!mIsNewNote){
+            displayNote(mSpCourses, mEdtNoteTitle, mEdtNoteText);
+        }
+
+    }
+
+    private void saveOriginalNoteValues() {
+        if(mIsNewNote)
+            return;
+        mViewModel.mOriginalNoteCourseId = mNote.getCourse().getCourseId();
+        mViewModel.mOriginalNoteTitle = mNote.getTitle();
+        mViewModel.mOriginalNoteText = mNote.getText();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_send_mail:
+                sendMail();
+                return true;
+            case R.id.action_cancel:
+                mIsCancelling = true;
+                finish();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void displayNote(Spinner spCourses, EditText edtNoteTitle, EditText edtNoteText) {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseIndex  =courses.indexOf(mNote.getCourse());
+        spCourses.setSelection(courseIndex);
+        edtNoteTitle.setText(mNote.getTitle());
+        edtNoteText.setText(mNote.getText());
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+        int posittion = intent.getIntExtra(NoteActivity.NOTE_POSITION, POSITION_NOT_SET);
+        mIsNewNote = posittion ==POSITION_NOT_SET;
+        if(mIsNewNote){
+            createNewNote();
+        }
+        else{
+            mNote = DataManager.getInstance().getNotes().get(posittion);
+        }
+
+
+
+    }
+
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        mNotePosition = dm.createNewNote();
+        mNote = dm.getNotes().get(mNotePosition);
+    }
+
+    private void sendMail() {
+        CourseInfo course = (CourseInfo) mSpCourses.getSelectedItem();
+        String subject = mEdtNoteTitle.getText().toString();
+        String text =   "Checkout what I learned in Pluralsight course \"" + course.getTitle()
+                + "\"\n" + mEdtNoteText.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc2822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        if(mIsCancelling){
+            if (mIsNewNote)
+                DataManager.getInstance().removeNote(mNotePosition);
+            else {
+                restorePreviousNoteValues();
+            }
+        }
+        else{
+            saveNote();
+        }
+        super.onPause();
+    }
+
+    private void restorePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(mViewModel.mOriginalNoteCourseId);
+        mNote.setCourse(course);
+        mNote.setTitle(mViewModel.mOriginalNoteTitle);
+        mNote.setText(mViewModel.mOriginalNoteText);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(outState!=null)
+            mViewModel.saveState(outState);
+    }
+
+    private void saveNote() {
+        mNote.setCourse((CourseInfo) mSpCourses.getSelectedItem());
+        mNote.setTitle(mEdtNoteTitle.getText().toString());
+        mNote.setText(mEdtNoteText.getText().toString());
+    }
+}
--- app/src/main/java/com/example/notekeeper/NoteListAcitivity.java
+++ app/src/main/java/com/example/notekeeper/NoteListAcitivity.java
@@ -1,6 +1,7 @@
 package com.example.notekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,35 +10,54 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 
 public class NoteListAcitivity extends AppCompatActivity {
 
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note_list_acitivity);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NoteListAcitivity.this, NoteActivity.class));
+            }
+        });
 
         connectViews();
     }
 
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        mAdapterNotes.notifyDataSetChanged();
+    }
     private void connectViews() {
         final ListView listNotes = findViewById(R.id.list_notes);
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
 
-        listNotes.setAdapter(adapterNotes);
+        listNotes.setAdapter(mAdapterNotes);
 
         listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(NoteListAcitivity.this, MainActivity.class);
-                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-                intent.putExtra(MainActivity.NOTE_INFO, note);
+                Intent intent = new Intent(NoteListAcitivity.this, NoteActivity.class);
+                intent.putExtra(NoteActivity.NOTE_POSITION, position);
 
                 startActivity(intent);
             }
         });
     }
+
 }
